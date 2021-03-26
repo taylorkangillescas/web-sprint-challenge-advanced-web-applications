0a30065390676583736a5c5861cd4dcc6a911209
Taylor Kang-Illescas <taylorkangillescas@Taylors-MacBook-Pro-8.local>
2021-03-26 15:36:26 -0700

    updated, may need changes to bubblepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from './components/PrivateRoute';
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,6 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <PrivateRoute path="/bubble" component={BubblePage}/>
       </div>
     </Router>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,69 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-const Login = () => {
+
+const initState = {
+  username: 'Lambda School',
+  password: 'i<3Lambd4',
+}
+
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [form, setForm] = useState(initState);
+  const {push} = props.history
 
-  useEffect(()=>{
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    axios.post('http://localhost:5000/api/login', form)
+    .then((res)=>{
+      localStorage.setItem('token', res.data.payload)
+      push('/bubble-page')
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  
     // make a post request to retrieve a token from the api
     // when you have handled the token, navigate to the BubblePage route
-  });
+  
   return (
     <>
       <h1>
         Welcome to the Bubble App!
         <p>Build a login page here</p>
       </h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor='username'>
+          <input
+          type='text'
+          id='username'
+          placeholder='username'
+          onChange={handleChange}
+          value={form.username}
+          />
+        </label>
+        <label htmlFor='password'>
+          <input
+          type='text'
+          id='password'
+          placeholder='password'
+          onChange={handleChange}
+          value={form.password}
+          />
+        </label>
+        <button>Login</button>
+      </form>
     </>
   );
 };
